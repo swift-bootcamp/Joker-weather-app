e25f0429e48220c9e8963e46552821ef10c492c6
JokerChuang <Joker@JokerChuangdeMacBook-Air.local>
2014-08-17 17:04:24 +0800

    add switch case

--- WeatherAPP II/ViewController.swift
+++ WeatherAPP II/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController, NSURLConnectionDataDelegate{
     
     @IBOutlet var city: UILabel!
     @IBOutlet var icon: UIImageView!
-    @IBOutlet var temperature: UILabel!
+    @IBOutlet var temperature: UILabel?
+    @IBOutlet var weathericon: UIImageView!
+    @IBOutlet var humidity: UILabel!
+    @IBOutlet var speed: UILabel!
     
     //使用 NSMutableData 儲存下載資料
     var data : NSMutableData = NSMutableData()
@@ -28,6 +31,8 @@ class ViewController: UIViewController, NSURLConnectionDataDelegate{
         
         self.city.text = "Taipei"
         self.icon.image = UIImage(named: "rainy")
+        self.weathericon.image = UIImage(named: "04d.png")
+        
         
         let singleFingerTap = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
         self.view.addGestureRecognizer(singleFingerTap)
@@ -75,16 +80,58 @@ class ViewController: UIViewController, NSURLConnectionDataDelegate{
         
         // 使用 NSDictionary: NSDitionary 是一種 Associative Array 的資料結構
         var error: NSError?
-        let jsonDictionay = NSJSONSerialization.JSONObjectWithData(self.data, options: NSJSONReadingOptions.MutableContainers, error: &error) as NSDictionary
+        let jsonDictionay = NSJSONSerialization.JSONObjectWithData(self.data, options: NSJSONReadingOptions.MutableContainers, error: &error) as NSDictionary                       // & 符號 - 傳遞參考
         
         // 讀取各項天氣概況
         let temp: AnyObject? = jsonDictionay["main"]?["temp"]
+        let humidity: AnyObject? = jsonDictionay["main"]?["humidity"]
+        let speed: AnyObject? = jsonDictionay["wind"]?["speed"]
+        
+        
+        // 讀取即時天氣圖示
+        if let weather = jsonDictionay["weather"]? as? NSArray {
+            // safe code 寫作概念
+            // 使用 as? 轉型時要把以下這行放進 if statement 裡處理
+            let weatherDictionary = (weather[0] as NSDictionary)
+            // 天氣狀態 (多雲 , 晴朗等等)
+            let weatherID = weatherDictionary["id"] as Int
+           // println("weather ID: \(weatherID)")
+  
+            switch weatherID {
+            case 500, 501, 502, 503, 504:
+                self.icon.image = UIImage(named: "RAIN.png")
+                self.weathericon.image = UIImage(named: "rain_s.png")
+                let background = UIImage(named: "rain_bg320px.png")
+                self.view.backgroundColor = UIColor(patternImage: background)
+                
+            case 800, 801, 802 , 803:
+                self.icon.image = UIImage(named: "sun.png")
+                let background = UIImage(named: "sun_bg320px.png")
+                self.weathericon.image = UIImage(named: "sun_s.png")
+                self.view.backgroundColor = UIColor(patternImage: background)
+                
+            case 804:
+                self.icon.image = UIImage(named: "CloudyDay.png")
+                self.weathericon.image = UIImage(named: "cloudDay_s-2.png")
+                let background = UIImage(named: "CloudyDay_bg320px.png")
+                self.view.backgroundColor = UIColor(patternImage: background)
+                
+            default:
+                println("unknow weather icon")
+            }
+
+            }
         
         // 資料處理
         let weatherTempCelsius = Int(round((temp!.floatValue - 273.15)))
         let weatherTempFahrenheit = Int(round(((temp!.floatValue - 273.15) * 1.8) + 32))
-        
+        let weatherHumidity = round(humidity!.floatValue)
+        let weatherSpeed = round(speed!.floatValue)
+
         // 輸出到 UI
-        self.temperature.text = "\(weatherTempCelsius)℃"
+        self.temperature!.text = "\(weatherTempCelsius)℃"
+        self.humidity.text = "\(weatherHumidity)%"
+        self.speed.text = "\(weatherSpeed) m/s"
+        
     }
 }
